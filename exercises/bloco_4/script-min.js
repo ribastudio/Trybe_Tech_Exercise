8b61815336113929d7c41991aaadc7c1daa0182c
Riba Jr <ribastudio@gmail.com>
2021-06-25 16:04:53 -0300

    Organizaçao da branch master e organização dos exercícios

---
+++ exercises/bloco_4/script-min.js
@@ -0,0 +1,1 @@
+let tasksList=["Tomar café","Reunião","Brincar com o cachorro"];tasksList.push("Fazer exercícios da Trybe");
---
+++ exercises/bloco_4/script.js
@@ -0,0 +1,15 @@
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let ones of names) {
+//     console.log(ones);
+// }
+
+let lista = [];
+
+for(let index = 0; index <= 25; index += 1) {
+    lista.push(index);
+}
+
+for(let index = 0; index < lista.length; index += 1) {
+    
+}
